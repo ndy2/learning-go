c8ddc86bdae31ae5f5ae15ee74a503c6ed1bd655
ndy2 <emrdbs12@gmail.com>
2024-10-13 21:12:27 +0900

    learning-go section 4.2 if, 4.3 for

---
+++ codes/chapter04/if/if_variable_scope.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	if n := rand.Intn(10); n == 0 {
+		fmt.Println("That's too low")
+	} else if n > 5 {
+		fmt.Println("That's too big:", n)
+	} else {
+		fmt.Println("That's a good number:", n)
+	}
+
+	// Unresolved reference 'n'
+	// fmt.Println(n)
+}
