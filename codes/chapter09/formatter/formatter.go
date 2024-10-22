e9af792bbbf00f75eb913d516f805d18d017d346
deukyun <emrdbs12@gmail.com>
2024-10-22 19:53:13 +0900

    learning-go 9.3 - Building Packages

--- codes/chapter09/formatter/formatter.go
+++ codes/chapter09/formatter/formatter.go
@@ -2,6 +2,7 @@ package print
 
 import "fmt"
 
+// Format returns a formatted string
 func Format(num int) string {
 	return fmt.Sprintf("The number is %d", num)
 }
--- codes/chapter09/math/math.go
+++ codes/chapter09/math/math.go
@@ -1,5 +1,6 @@
 package math
 
+// Double returns the double of an integer
 func Double(x int) int {
 	return x * 2
 }
